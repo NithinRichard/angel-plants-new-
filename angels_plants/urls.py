2813363513caff75e5efd702008ffe1d1b273175
unknown <nithinrichard1@gmail.com>
2025-05-28 11:02:30 +0530

    Update urls.py with catch-all pattern for debugging

--- angels_plants/urls.py
+++ angels_plants/urls.py
@@ -4,6 +4,11 @@ from django.http import HttpResponse
 def test_view(request):
     return HttpResponse("Test view is working!")
 
+def catch_all(request, path=''):
+    return HttpResponse(f"Path requested: {path}")
+
 urlpatterns = [
     path('test/', test_view, name='test'),
+    path('', catch_all, name='home'),
+    path('<path:path>', catch_all),  # Catch all other paths
 ]
